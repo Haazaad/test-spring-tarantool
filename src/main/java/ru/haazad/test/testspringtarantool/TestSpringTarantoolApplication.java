11e648795bde33d768d64ff87daf29cfbbd4d9d0
Artem.Smirnov <artem.smirnov@nexign.com>
2022-09-12 16:29:09 +0400

    Initial commit

---
+++ src/main/java/ru/haazad/test/testspringtarantool/TestSpringTarantoolApplication.java
@@ -0,0 +1,13 @@
+package ru.haazad.test.testspringtarantool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringTarantoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringTarantoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/haazad/test/testspringtarantool/TestSpringTarantoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.haazad.test.testspringtarantool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringTarantoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
